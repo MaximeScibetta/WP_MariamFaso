41611fbe8fed77bdc9b082795bdc2b669a7cb39c
Maxime Scibetta <maxime.scibetta@outlook.com>
2017-05-17 18:33:07 +0200

    Create triple stats template

---
+++ wp-content/themes/mariamFaso/part/tripleStats.php
@@ -0,0 +1,23 @@
+    <div>
+        <div class="stat">
+            <canvas id="asblStat1" width="250" height="250"> </canvas>
+        </div>
+        <p>Pour mettre en oeuvre ce projet nous avons besoins de certains nombre d'outils pour nous aider à construire le puits.</p>
+    </div>
+    <div>
+        <div class="stat">
+            <canvas id="asblStat2" width="400" height="400"> </canvas>
+        </div>
+        <p>Pour mettre en oeuvre ce projet nous avons besoins de certains nombre d'outils pour nous aider à construire le puits.</p>
+    </div>
+    <div>
+        <div class="stat">
+            <canvas id="asblStat3" width="400" height="400"> </canvas>
+        </div>
+        <p>Pour mettre en oeuvre ce projet nous avons besoins de certains nombre d'outils pour nous aider à construire le puits.</p>
+    </div>
+    
+<script src="<?= dw_asset('js/Chart.js');?>"></script>
+<script src="<?= dw_asset('js/statistics.js');?>"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.5.0/Chart.bundle.min.js"></script>
+</section>
