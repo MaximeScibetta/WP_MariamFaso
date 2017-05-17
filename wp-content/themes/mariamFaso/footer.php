95db49f50b5cd6d7714787ff12a04c324927b4cc
Maxime Scibetta <maxime.scibetta@outlook.com>
2017-05-17 18:30:29 +0200

    Prepare footer for get_footer()

---
+++ wp-content/themes/mariamFaso/footer.php
@@ -0,0 +1,8 @@
+    </article>
+    <footer class="foot">
+        <p class="foot__author">Par Maxime Scibetta</p>
+    </footer>
+    <?php if( is_user_logged_in()) wp_footer(); ?>
+</body>
+<script src="<?= dw_asset('js/sidebar.js');?>"></script>
+</html>
