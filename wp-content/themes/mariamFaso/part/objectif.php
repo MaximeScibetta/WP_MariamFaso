d0ea3cb37c72742b03690578e65db89b908c8bb4
Maxime Scibetta <maxime.scibetta@outlook.com>
2017-05-17 18:33:52 +0200

    Create objectiv template

---
+++ wp-content/themes/mariamFaso/part/objectif.php
@@ -0,0 +1,8 @@
+            <?php $fields = get_fields(); ?>
+            <h3>Notre <span>objectif</span></h3>
+            <div class="text">
+                <p><?= $fields['objectiv']; ?></p>
+                <a href="./contact.html" class="btn">Participer</a>
+            </div>
+        </section>
+        
