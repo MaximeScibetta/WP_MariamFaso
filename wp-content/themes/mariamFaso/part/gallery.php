adcec26d2d0264afda418955311c829231ad0d3f
Maxime Scibetta <maxime.scibetta@outlook.com>
2017-05-17 18:32:31 +0200

    Create gallery template

---
+++ wp-content/themes/mariamFaso/part/gallery.php
@@ -0,0 +1,49 @@
+                <h3>En images ...</h3>
+                <p>TEXTTEXTTEXTTEXTTEXTTEXTTEXTTEXTTEXTTEXTTEXTTEXTTEXTTEXTTEXTTEXTTEXTTEXT</p>
+                <a href="./actu.html" class="btn">Nos projets</a>
+                <div id="sequence" class="seq">
+                    <div class="seq-screen">
+                        <ul class="seq-canvas">
+
+                            <li class="seq-step1 seq-in" id="step1">
+                                <div class="seq-content">
+                                    <img src="./assets/images/ajax1.jpg" alt="">
+                                </div>
+                            </li>
+
+                            <li class="seq-step2" id="step2">
+                                <div class="seq-content">
+                                    <img src="../images/ajax2.jpg" alt="">
+                                </div>
+                            </li>
+
+                            <li class="seq-step3" id="step3">
+                                <div class="seq-content">
+                                    <img src="../images/ajax3.jpg" alt="">
+                                </div>
+                            </li>
+
+                            <li class="seq-step4" id="step4">
+                                <div class="seq-content">
+                                    <img src="../images/ajax1.jpg" alt="">
+                                </div>
+                            </li>
+
+                            <li class="seq-step5" id="step5">
+                                <div class="seq-content">
+                                    <img src="../images/ajax2.jpg" alt="">
+                                </div>
+                            </li>
+                        </ul>
+                    </div>
+
+                    <ul rel="sequence" class="seq-pagination" role="navigation" aria-label="Pagination">
+                        <li><a href="#step1" rel="step1" title="Go to step 1">Step 1</a></li>
+                        <li><a href="#step2" rel="step2" title="Go to step 2">Step 2</a></li>
+                        <li><a href="#step3" rel="step3" title="Go to step 3">Step 3</a></li>
+                        <li><a href="#step4" rel="step4" title="Go to step 4">Step 4</a></li>
+                        <li><a href="#step5" rel="step5" title="Go to step 5">Step 3</a></li>
+                    </ul>
+
+                </div>
+            </section>
